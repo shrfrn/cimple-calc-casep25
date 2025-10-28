c993dcd7c354d496c4820a80f71119756ca42c71
shrfrn <shrfrn@gmail.com>
2025-10-28 10:42:06 +0200

    Add event hadler placeholders

---
+++ js/simple-calc.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onNumClick(elCell) {
+    console.log('Hi', elCell.innerText)
+}
+
+function onOperatorClick(elCell) {
+    console.log('Hi', elCell.innerText)
+}
+
+function onCalc() {
+    console.log('Calculating')
+}
